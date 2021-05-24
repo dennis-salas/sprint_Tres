c77f8eaf1f55df6628fdcab85826dccf8e262cb4
Dennis Salas <dgsl93.ds@gmail.com>
2021-05-24 17:54:30 -0500

    pagina ecommerce

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 //variables
 let formulario = document.querySelector('#formulario');
-
+let verificarDatos = document.querySelector('#verificarDatos');
 let arreglo =  [];
 
 //funciones
@@ -17,6 +17,7 @@ const crearRegistro = (nombreC,correoE,numeroT,fechaV,cvv) => {
 
     return registro;
 }
+
 /*let r1 = crearRegistro(1,5,9,7,4);
 let r2 = crearRegistro("hi","hola","hello","chao","bay");
 console.log(arreglo);*/
@@ -25,7 +26,32 @@ const guardarDatos = () => {
     localStorage.setItem('Datos Cliente', JSON.stringify(arreglo))
 }
 
-
+const alertaVerificarDatos = () => {
+    
+    verificarDatos.innerHTML = "";
+    arreglo = JSON.parse(localStorage.getItem('Datos Cliente'));
+    console.log(arreglo);
+    
+    arreglo === null ? (arreglo = []) : (
+       arreglo.forEach(element =>{
+          verificarDatos.innerHTML += `<div class="alert alert-info" role="alert">
+    <i class="material-icons float-left mr-2" >
+    </i>
+    <img src="./images/iconos/info-lg.svg" alt="">
+        <b>${element.nombreCompleto}</b>
+        <b>${element.correoElectronico}</b>
+        <b>${element.numeroTarjeta}</b>
+        <b>${element.fechaVencimiento}</b>
+        <b>${element.cvv}</b>
+      <b>Verificar Datos</b>
+      <span class="float-right">
+          <i class="material-icons"><img src="./images/iconos/check-circle-fill.svg" alt=""></i>
+          <i class="material-icons"><img src="./images/iconos/trash.svg" alt=""></i>
+      </span>
+</div> `
+    })
+    );
+}
 
 //Eventos
 formulario.addEventListener('submit', e => {
@@ -41,4 +67,14 @@ formulario.addEventListener('submit', e => {
     guardarDatos();
 
     formulario.reset();
+})
+document.addEventListener('DOMContentLoaded',alertaVerificarDatos);
+
+verificarDatos.addEventListener('click', (e) => {
+    e.preventDefault();
+    //console.log(e)
+    //console.log(e.path[3].childNodes[5].innerHTML);
+    console.log(e.target.innerHTML);
+/// no  me imprime el delete
+
 })
